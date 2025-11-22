03290a57e751dc2ce997a823c27fa9f088163ec9
roadjennie-git <roadjennie@gmail.com>
2025-11-22 14:55:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const express = require('express');
 const cors = require('cors');
 const geolib = require('geolib');
 const { decode } = require('@googlemaps/polyline-codec');
+const path = require("path");
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+module.exports = app;
 
 const serviceAccount = {
   type: 'service_account',
